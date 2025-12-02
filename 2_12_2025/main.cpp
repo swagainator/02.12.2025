b2dbe998df0282f84055f279262d833432bfbcc3
swagainator <platonalekseevi6@gmail.com>
2025-12-02 10:27:31 +0300

    main begin

--- 2_12_2025/main.cpp
+++ 2_12_2025/main.cpp
@@ -15,6 +15,7 @@ namespace topit {
     bool operator==(p_t a, p_t b);
     bool operator!=(p_t a, p_t b);
     struct Dot : IDraw {
+        explicit Dot(p_t dd);
         p_t begin() const override;
         p_t next(p_t prev) const override;
         p_t d;
@@ -22,9 +23,25 @@ namespace topit {
 }
 int main() {
     using namespace topit;
-    p_t a{ 1,1 }, b{ 0,1 };
-    std::cout << (a == b) << "\n";
+    int err = 0;
+    IDraw* shp[3] = {};
+    try {
+        shp[0] = new Dot({ 0, 0 });
+        shp[1] = new Dot({ 2,3 });
+
+    } catch (...) {
+        std::cerr << "ERR\n";
+        err = 1;
+    }
+    
+    delete shp[1];
+    delete shp[0];
+    return err;
 }
+topit::Dot::Dot(p_t dd) :
+ IDraw(), 
+ d{dd} 
+{}
 topit::p_t topit::Dot::begin() const {
     return d;
 }
