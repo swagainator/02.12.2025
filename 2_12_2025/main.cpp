98b3b000033008395485fc32005b2e241cccebf8
swagainator <platonalekseevi6@gmail.com>
2025-12-02 09:37:08 +0300

    operators and namespace

--- 2_12_2025/main.cpp
+++ 2_12_2025/main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
-int main(){}
+namespace topit {
+    struct p_t {
+        int x, y;
+    };
+
+    bool operator==(p_t a, p_t b);
+    bool operator != (p_t a, p_t b);
+}
+int main() {
+    using namespace topit;
+    p_t a{ 1,1 }, b{ 0,1 };
+    std::cout << (a == b) << "\n";
+}
+
+bool topit::operator==(p_t a, p_t b) {
+    return a.x = b.x && a.y == b.y;
+}
+bool topit::operator != (p_t a, p_t b) {
+    return !(a == b);
+}
